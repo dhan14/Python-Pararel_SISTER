9f65be86f290bf54c6c968e05668c1b15372490a
dhan14 <yaqdhan.ariana@gmail.com>
2022-03-23 11:55:35 +0700

    Penjelasan teknis file.py

--- Chapter01/file.py
+++ Chapter01/file.py
@@ -1,11 +1,11 @@
-f = open ('test.txt', 'w')
-f.write ('first line of file \n') 
+f = open ('test.txt', 'w') #membaca file di luar
+f.write ('first line of file \n') #menulis ke dalam file
 
-f.write ('second line of file \n') 
+f.write ('second line of file \n') #menulis ke dalam file
 
-f.close()
-f = open ('test.txt')
+f.close()#keluarkan file nya
+f = open ('test.txt') #buka file
 content = f.read()
-print (content)
+print (content) #render content yang ada di dalam file nya
 
 f.close()
